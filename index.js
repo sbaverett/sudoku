c5bd15fae2e3f79cf89ce73fa91a179f323072df
Ariel Spear <simpleredfox@gmail.com>
2014-05-28 13:47:52 -0700

    tests pass, hurray high-five

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-// var row1 = [1, 5, 8, ' ', 2, ' ', 6, ' ', 2];
+
+
+
 module.exports.checkRow = function(puzzle, row, comparedNumber){
-// get the first nine characters from puzzle...
-// put those into a array[]
-// iterate thru row and compare to comparedNumber and return result
-var row1 = puzzle.substr(0, 9);
-	return true;
-console.log(row1);
+	// get the first nine characters from puzzle...
+	// put those into a array[]
+	// iterate thru row and compare to comparedNumber and return result
+	
+	var result = false;
+	var row1 = puzzle.substr(0, 9);
 
+	row1 = row1.split('');
+	console.log("+%j+", row1);
+	row1.forEach(function(number){
+		number = parseInt(number);
+		console.log(number);
+		if (number === comparedNumber){
+			result = true;
+		}
+	});
+	return result;
 };
-
