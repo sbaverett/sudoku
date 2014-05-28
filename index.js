6b94c5decc9579a6ca9c71dd807cba952a469ddd
Ariel Spear <simpleredfox@gmail.com>
2014-05-28 14:10:12 -0700

    Tests for row2 passing, and all rows should pass at this point

--- index.js
+++ index.js
@@ -8,13 +8,32 @@ module.exports.checkRow = function(puzzle, row, comparedNumber){
 	
 	var result = false;
 	var row1 = puzzle.substr(0, 9);
+	var row2 = puzzle.substr(9, 9);
+	var row3 = puzzle.substr(18, 9);
+	var row4 = puzzle.substr(27, 9);
+	var row5 = puzzle.substr(36, 9);
+	var row6 = puzzle.substr(45, 9);
+	var row7 = puzzle.substr(54, 9);
+	var row8 = puzzle.substr(63, 9);
+	var row9 = puzzle.substr(72, 9);
 
-	row1 = row1.split('');
-	console.log("+%j+", row1);
-	row1.forEach(function(number){
+	if (row === 1) row = row1;
+	if (row === 2) row = row2;
+	if (row === 3) row = row3;
+	if (row === 4) row = row4;
+	if (row === 5) row = row5;
+	if (row === 6) row = row6;
+	if (row === 7) row = row7;
+	if (row === 8) row = row8;
+	if (row === 9) row = row9;
+
+
+	row = row.split('');
+	//console.log("+%j+", row1);
+	row.forEach(function(number) {
 		number = parseInt(number);
-		console.log(number);
-		if (number === comparedNumber){
+	//	console.log(number);
+		if (number === comparedNumber) {
 			result = true;
 		}
 	});
--- test/test.js
+++ test/test.js
@@ -11,6 +11,8 @@ describe('checkRow1()', function() {
 
 	var puzzle = '158 2  6 2   8  9  3  7 8 2 6 74      4 6 7      19 5 4 9 3  2  2  5   8 7  9 413';
 	
+	//row1
+
 	it('should check row1 for number 1', function() {
 		expect(_.checkRow(puzzle, 1, 1)).to.eql(true);
 	});
@@ -24,4 +26,21 @@ describe('checkRow1()', function() {
 	it('should check row1 for number 4', function() {
 		expect(_.checkRow(puzzle, 1, 4)).to.eql(false);
 	});
+	
+	//row2
+
+	it('should check row2 for number 1', function() {
+		expect(_.checkRow(puzzle, 2, 1)).to.eql(false);
+	});
+
+	it('should check row2 for number 2', function() {
+		expect(_.checkRow(puzzle, 2, 2)).to.eql(true);
+	});
+	it('should check row2 for number 3', function() {
+		expect(_.checkRow(puzzle, 2, 3)).to.eql(false);
+	});
+	it('should check row2 for number 4', function() {
+		expect(_.checkRow(puzzle, 2, 4)).to.eql(false);
+	});
+
 });
